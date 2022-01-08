ac9b12399a9312492c00dcb7659a963cc468ab10
4498Lincoln <49529648+4498Lincoln@users.noreply.github.com>
2022-01-08 11:10:54 -0800

    Update LuxbotFramework.ts

--- LuxbotFramework.ts
+++ LuxbotFramework.ts
@@ -23,77 +23,92 @@ developing sprites easier.
 
 // Luxbot NPC Type
 namespace SpriteKind {
-    export const LuxbotKind = SpriteKind.create();
+    export const LuxbotKind = SpriteKind.create()
 }
 
 // Luxbot class
 class LBChar {
     // VARIABLES //
-    private sprite: Sprite;
-    private kind: string;
+    private sprite: Sprite
+    private kind: string
     
-    private doFollowAdv: boolean;
+    private doFollowAdv: boolean
 
     ///////////////
 
     // Constructor
     constructor(img: Image, kind: string) {
-        this.kind = kind;
-        this.sprite = sprites.create(img, SpriteKind.LuxbotKind);
+        this.kind = kind
+        this.sprite = sprites.create(img, SpriteKind.LuxbotKind)
         
-        this.doFollowAdv = false;
+        this.doFollowAdv = false
     }
 
     // Set image
     setImg(newImg: Image) {
-        this.sprite.setImage(newImg);
+        this.sprite.setImage(newImg)
     }
     // Get image
     getImg() {
-        return this.sprite.image;
+        return this.sprite.image
     }
     // Set kind
     setKind(newKind: string) {
-        this.kind = newKind;
+        this.kind = newKind
     }
     // Get kind
     getKind() {
-        return this.kind;
+        return this.kind
     }
     // Get sprite
     getSprite() {
-        return this.sprite;
+        return this.sprite
     }
     // Follow advanced
-    followAdv(toFollow: Sprite, speedX: number, speedY: number) {
-        this.doFollowAdv = true;
+    followAdv(params: {target: Sprite, speedX: number, speedY: number, jumpVel: number}) {
+        this.doFollowAdv = true
         control.runInParallel(function() {
             do {
                 // Positions of bot and target
-                const botX = this.sprite.x;
-                const botY = this.sprite.y;
-                const tarX = toFollow.x;
-                const tarY = toFollow.y;
+                const botX = this.sprite.x
+                const botY = this.sprite.y
+                const tarX = params.target.x
+                const tarY = params.target.y
 
                 // Calculate desired velocity
-                const xVelNew = speedX * Math.sign(tarX - botX);
-                const yVelNew = speedY * Math.sign(tarY - botY);
+                const xVelNew = params.speedX * Math.sign(tarX - botX)
+                const yVelNew = params.speedY * Math.sign(tarY - botY)
 
                 // Set velocity of enemy
-                this.sprite.vx = xVelNew;
-                this.sprite.vy = yVelNew;
+                if (params.speedX != 0) this.sprite.vx = xVelNew
+                if (params.speedY != 0) this.sprite.vy = yVelNew
+
+                // Jump
+                if (this.sprite.isHittingTile(CollisionDirection.Left) || this.sprite.isHittingTile(CollisionDirection.Right)) {
+                    console.log("Touching left/right wall")
+                    if (this.sprite.isHittingTile(CollisionDirection.Bottom)) {
+                        console.log("Touching bottom wall")
+                        console.log(params.jumpVel)
+                        this.sprite.vy = params.jumpVel
+                    }
+                }
 
                 pause(50)
-            } while (this.doFollowAdv);
-        });
+            } while (this.doFollowAdv)
+        })
     }
     // Cancel follow advanced
     undoFollowAdv() {
-        this.doFollowAdv = false;
+        this.doFollowAdv = false
     }
     // Doing follow advanced
     doingFollowAdv() {
-        return this.doFollowAdv;
+        return this.doFollowAdv
+    }
+    
+    // Get tilemap position
+    getTilePos() {
+        return tiles.getTileLocation(Math.floor(this.sprite.x/16), Math.floor(this.sprite.y/16))
     }
 }
 
@@ -103,35 +118,35 @@ class LBChar {
 
 // Joulebot character list class
 class LBCharList {
-    private charList: Array<LBChar>;
+    private charList: Array<LBChar>
 
     // Constructor
     constructor() {
-        this.charList = [];
+        this.charList = []
     }
 
     // New Luxbot
     createCharacter(img: Image, kind: string) {
-        let newChar = new LBChar(img, kind);
-        this.charList.push(newChar);
-        return newChar;
+        let newChar = new LBChar(img, kind)
+        this.charList.push(newChar)
+        return newChar
     }
 
     // List of characters
     allCharacters() {
-        return this.charList;
+        return this.charList
     }
 
     // Find characters of kind
     charactersOfKind(kindToFind: string) {
-        let returnList: Array<LBChar> = [];
+        let returnList: Array<LBChar> = []
 
         this.charList.forEach(function (val) {
             if (val.getKind() == kindToFind) {
-                returnList.push(val);
+                returnList.push(val)
             }
         })
 
-        return returnList;
+        return returnList
     }
 }
